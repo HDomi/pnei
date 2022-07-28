f1da14e5e76fba5578ca076af56b91da9c3bea4e
HDomi <84692508+HDomi@users.noreply.github.com>
2022-07-28 16:58:13 +0900

    ㅁㅁㅁ

---
+++ js/modal.js
@@ -0,0 +1,10 @@
+$(function() {
+            $(".nav_box a p.txt1").click(function() {
+                $("#modal").fadeIn();
+            });
+
+            $("div.search p.button").click(function() {
+                $("#modal").fadeOut();
+            });
+            
+            });
